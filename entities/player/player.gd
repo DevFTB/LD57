7503ea2ed9db8c4d865ca10f6eeec5b6fe54668c
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-05 18:04:09 +1100

    added throwable bombs

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,12 +1,36 @@
 extends GroundedCharacterController
 class_name Player
 
+class ThrowInitiatedEventData:
+    var position: Vector2
+    var impulse: Vector2
+    var bomb_scene: PackedScene
+    
+
 signal interacted
+signal throw_initiated(data: ThrowInitiatedEventData)
+
+const THROWABLE_BOMB := preload("uid://do0g7votir2b4")
+
+@export var throw_force_curve: Curve
 
 @onready var inventory_component: InventoryComponent = $InventoryComponent
 
 func _physics_process(delta: float) -> void:
-	super._physics_process(delta)
-	
-	if Input.is_action_just_pressed("interact"):
-		interacted.emit()
+    super._physics_process(delta)
+    
+    if Input.is_action_just_pressed("interact"):
+        interacted.emit()
+    
+    if Input.is_action_just_pressed("throw"):
+        initiate_throw()
+
+func initiate_throw() -> void:
+    var direction := global_position.direction_to(get_global_mouse_position())
+
+    var data := ThrowInitiatedEventData.new()
+    data.position = global_position
+    data.impulse = direction.normalized() * throw_force_curve.sample_baked(1.0)
+    data.bomb_scene = THROWABLE_BOMB
+
+    throw_initiated.emit(data)
