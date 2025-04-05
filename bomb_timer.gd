266cd471418a6483f98305a79c91229a7a5267ce
Arthur <thepandaparadox@gmail.com>
2025-04-05 13:50:46 +1100

    clamps max value

--- bomb_timer.gd
+++ bomb_timer.gd
@@ -8,7 +8,8 @@ extends Node2D
 @export var bomb_timer_starting_value : float = 300 #time in seconds for bomb to explode
 
 var bomb_max_value : float
-var bomb_timer : float
+var bomb_timer : float:
+	set(value) : clamp(value, 0, bomb_max_value)
 
 var is_running = true
 
@@ -29,8 +30,6 @@ func _process(delta):
 	var minutes = floor(bomb_time_left / 60)
 	var seconds = floor(bomb_time_left - (minutes * 60))
 	time_display.text = (str(int(minutes)) + ":" + str(int(seconds)))
-	pass
 
-func _on_bomb_timer_timeout():
-	print("Game is over, bomb explodied sadge")
-	pass # Replace with function body.
+	if bomb_timer <= 0:
+		print("Game is over, bomb explodied sadge")
