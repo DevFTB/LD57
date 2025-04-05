a768639fc2d7b6c3ac08a658fc8717e57fdeb99d
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-06 00:23:31 +1100

    added repeating hit damage when staying in hitbox

--- entities/common/hitbox_component.gd
+++ entities/common/hitbox_component.gd
@@ -1,11 +1,25 @@
 extends Area2D
 class_name HitboxComponent
 
-@export var damage : int = 1
+@export var damage: int = 1
+
+@export var repeating := false
+@export var repeat_interval := 1.0
+
+@onready var hit_timer: Timer = $HitTimer
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	hit_timer.timeout.connect(_on_hit_timer_timeout)
 	
 func _on_area_entered(area: Area2D) -> void:
 	if area is HurtboxComponent:
 		area.apply_damage(damage, self)
+		hit_timer.start()
+		
+func _on_hit_timer_timeout() -> void:
+	var areas := get_overlapping_areas()
+	for area in areas:
+		if area is HurtboxComponent:
+			area.apply_damage(damage, self)
+			hit_timer.start()
---
+++ ui/health/health_bar.gd
@@ -0,0 +1,20 @@
+extends Range
+
+@export var health_component : HealthComponent
+
+func _ready() -> void:
+	set_health_component(health_component)
+
+func set_health_component(hc : HealthComponent) -> void:
+	health_component = hc
+	max_value = hc.maximum_health
+	value = hc.current_health
+	
+	hc.health_modified.connect(_on_health_modified)
+	hc.maximum_health_modified.connect(_on_maximum_health_modified)
+	
+func _on_health_modified(_amount: int, current_health: int) -> void:
+	value = current_health
+	
+func _on_maximum_health_modified(maximum_health: int) -> void:
+	max_value = maximum_health
