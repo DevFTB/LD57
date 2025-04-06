e90a0a5ec934afd2371fa53f3a2abf9a7e7f76ce
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-07 00:30:55 +1000

    Unified player death animation under animation player

--- entities/player/player_animation_tree.gd
+++ entities/player/player_animation_tree.gd
@@ -1,16 +1,27 @@
 extends AnimationTree
 
 @export var player: Player
+@export var health_component: HealthComponent
 @export var sprite: Sprite2D
 @onready var playback: AnimationNodeStateMachinePlayback = get("parameters/playback")
 
-
 func _ready() -> void:
 	player.jumped.connect(playback.travel.bind("jump"))
+	player.died.connect(_on_player_died)
+	health_component.health_modified.connect(_on_player_health_modified)
 	
 func _process(_delta: float) -> void:
 	sprite.flip_h = player.last_inputted_direction.x > 0
 	
 	if player._holding_throw or playback.get_current_node().contains("throw"):
 		sprite.flip_h = (player.get_global_mouse_position() - player.position).x > 0
-	# aprint(playback.get_current_node())
+	print(playback.get_current_node())
+
+func _on_player_health_modified(amount: int, _new_value: int) -> void:
+	print("tri", amount)
+	if amount < 0:
+		print("hurt")
+		playback.travel("hurt")
+
+func _on_player_died() -> void:
+	playback.travel("death")
