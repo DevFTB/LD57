36f9516d7f2c174035a270932591155247091795
Ryan <ryanwp526@gmail.com>
2025-04-06 18:37:52 +1000

    fix so skeleton bomb explodes when it hits player

--- entities/enemies/skeleton_bomber/skeleton_bomb.gd
+++ entities/enemies/skeleton_bomber/skeleton_bomb.gd
@@ -1,5 +1,5 @@
 extends ThrowableBomb
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is TileMapLayer or (body.get_parent() and body.get_parent().is_in_group("player")):
+	if body is TileMapLayer or (body.is_in_group("player")):
 		explode()
