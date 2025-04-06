98d3599f06cfaf8c03b12163d1a846dbb210c0e8
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-06 12:27:18 +1000

    Fixed broken signal

--- entities/throwables/bomb/bomb_explode.gd
+++ entities/throwables/bomb/bomb_explode.gd
@@ -1,9 +1,5 @@
 extends AnimatedSprite2D
 
 func _ready():
-	$AudioStreamPlayer2D.pitch_scale = randf_range(0.7,1.3)
-
-
-
-func _on_audio_stream_player_2d_finished():
-	queue_free()
+	$AudioStreamPlayer2D.finished.connect(queue_free)
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.7, 1.3)
