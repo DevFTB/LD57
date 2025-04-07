7e61b49b784f1fb31afd34fbe6b9350ef90286f7
Ryan <ryanwp526@gmail.com>
2025-04-08 02:01:40 +1000

    godot automatic changes

--- game.gd
+++ game.gd
@@ -6,12 +6,12 @@ extends Node
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimationPlayer/AnimatedSprite2D
 @onready var world: World = $World
 func _ready() -> void:
-    moab_explosion_system.timeout.connect(_on_moab_timeout)
+	moab_explosion_system.timeout.connect(_on_moab_timeout)
 
 const FED_FACTOR := 20.0
 func _on_moab_timeout() -> void:
-    world.freeze()
-    animation_player.play("game_over")
+	world.freeze()
+	animation_player.play("game_over")
 
-    var scale_factor: float = max(1, (StatsManager.get_stat(StatsManager.Stat.BOMBPOWDER_OFFERED) + stat_bonus) / FED_FACTOR)
-    animated_sprite_2d.scale = Vector2.ONE * scale_factor
+	var scale_factor: float = max(1, (StatsManager.get_stat(StatsManager.Stat.BOMBPOWDER_OFFERED) + stat_bonus) / FED_FACTOR)
+	animated_sprite_2d.scale = Vector2.ONE * scale_factor
