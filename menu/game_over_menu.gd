2f905848206d0dc02ffd372f97b1211c7c9cbc32
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-08 07:11:25 +1000

    game over states + fixed item entity

--- menu/game_over_menu.gd
+++ menu/game_over_menu.gd
@@ -1,4 +1,8 @@
 extends Control
+
+@export var win_controls: Array[Control] = []
+@export var lose_controls: Array[Control] = []
+
 @onready var game_over_exit_button: Button = %GameOverExitButton
 @onready var game_over_retry_button: Button = %GameOverRetryButton
 const MAIN_MENU: PackedScene = preload("res://menu/main_menu.tscn")
@@ -6,6 +10,12 @@ const MAIN_MENU: PackedScene = preload("res://menu/main_menu.tscn")
 func _ready() -> void:
 	game_over_exit_button.pressed.connect(quit)
 	game_over_retry_button.pressed.connect(retry)
+	
+func set_condition(did_win: bool) -> void:
+	for c in win_controls:
+		c.visible = did_win
+	for l in lose_controls:
+		l.visible = not did_win
 
 func quit() -> void:
 	get_tree().paused = false
