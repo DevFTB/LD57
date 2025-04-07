da066c99279a4195f2cd06b76798e87cca0e72c8
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-07 19:18:29 +1000

    Added item entity and tile chunking for explosions

--- entities/item/item_entity.gd
+++ entities/item/item_entity.gd
@@ -3,16 +3,35 @@ class_name ItemEntity
 
 @export var item: Item
 @export var quantity: int = 1
-
+var consumed := 0
 var pickup_sound_scene = preload("res://systems/music_sfx/files/sfx/ui/pickup_sound.tscn")
 
 @onready var player_detector: PlayerDetectorArea2D = $PlayerDetectorArea2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
-
+@onready var combination_area_2d: Area2D = $CombinationArea2D
 func _ready() -> void:
 	sprite_2d.texture = item.texture
 	player_detector.player_entered.connect(_on_player_entered)
 
+	$Timer.wait_time += randf()
+	$Timer.timeout.connect(consume, ConnectFlags.CONNECT_DEFERRED)
+
+func get_consumed() -> void:
+	if consumed > 0:
+		queue_free()
+
+func consume() -> void:
+	if not is_queued_for_deletion():
+		var other_item_entities := combination_area_2d.get_overlapping_bodies()
+		print(other_item_entities.size())
+		for item_entity: ItemEntity in other_item_entities:
+			if not item_entity.is_queued_for_deletion() and not item_entity == self:
+				if item_entity.item == item:
+					quantity += item_entity.quantity
+					item_entity.queue_free()
+					consumed += 1
+		
+	#prints(name, consumed)
 
 func _on_player_entered(player: Player) -> void:
 	if player.can_pickup:
