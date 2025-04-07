75fff9df1a7bf6d69421e5c7788dde1cce91927a
Aditha Doratiyawa <indegamedev@gmail.com>
2025-04-08 08:21:25 +1000

    fixed health component not updating on start of game

--- ui/HUD/label_health_bar.gd
+++ ui/HUD/label_health_bar.gd
@@ -2,6 +2,10 @@ extends HealthBar
 
 @export var label: Label
 
+func set_health_component(hc: HealthComponent) -> void:
+	super.set_health_component(hc)
+	label.text = str(hc.current_health)
+	
 func _on_health_modified(amount: int, current_health: int) -> void:
 	super._on_health_modified(amount, current_health)
 	
